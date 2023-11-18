b678acae27711eda12e9ffa2d46f22dccff6fe07
k33ngTomide <Msodiqakintomide@gmail.com>
2023-11-18 09:18:09 +0100

    DOM and BOM medium publish

---
+++ BOM/index.js
@@ -0,0 +1,20 @@
+
+// Accessing the window object
+let currentWindow = window;
+
+// Accessing the navigator object
+let browserInfo = navigator.userAgent;
+
+// Accessing the location object
+let currentURL = location.href;
+
+// Accessing the screen object
+let screenWidth = screen.width;
+let screenHeight = screen.height;
+
+// Displaying information in the console
+console.log("Window Object:", currentWindow);
+console.log("Browser Information:", browserInfo);
+console.log("Current URL:", currentURL);
+console.log("Screen Width:", screenWidth);
+console.log("Screen Height:", screenHeight);
---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+
+function action(){
+
+  // Accessing the heading element by its id
+  let headingElement = document.getElementById("heading");
+
+  // Modifying the text content of the heading
+  headingElement.innerText = "Hi";
+
+
+  let paragraphElement = document.getElementById("paragraph");
+  paragraphElement.innerText = "This is our room";
+}
